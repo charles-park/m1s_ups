53cbf0ae6df40ceb4d916a44e3d012114226c7f4
charles-park <charles.park@hardkernel.com>
2023-09-04 14:56:39 +0900

    service install bug fix

--- service/install_service.sh
+++ service/install_service.sh
@@ -4,30 +4,33 @@
 #/*---------------------------------------------------------------------------*/
 INSTALL_PATH="/root/m1s_ups"
 SYSTEM_PATH="/etc/systemd/system"
-SREVICE_NAME="m1s_ups"
+SERVICE_NAME="m1s_ups"
 SERVICE_SCRIPT="check_ups.sh"
 
 #/*---------------------------------------------------------------------------*/
 #/*---------------------------------------------------------------------------*/
 # service disable for update
-if [ $(systemctl is-active ${SERVICE_NAME}) == "active"]; then
+if [ $(systemctl is-active ${SERVICE_NAME}) == "active" ]; then
     echo "service ${SERVICE_NAME} active -> inactive"
-    service {$SERVICE_NAME} disable
+    service ${SERVICE_NAME} stop
+    # service disable
+    systemctl disable ${SERVICE_NAME}
 fi
 
 mkdir -p ${INSTALL_PATH}
 
-cp "${SERVICE_SCRIPT}" "${INSTALL_PATH}/"
-cp "${SERVICE_NAME}.service" "${SYSTEM_PATH}/"
+cp "${PWD}/${SERVICE_SCRIPT}" "${INSTALL_PATH}/"
+cp "${PWD}/${SERVICE_NAME}.service" "${SYSTEM_PATH}/"
 
 # service enable
-systemctl enable "${SERVICE_NAME}"
+systemctl enable ${SERVICE_NAME}
+
+# service restart
+service ${SERVICE_NAME} start
 
 # service status display
-systemctl status "${SERVICE_NAME}"
+systemctl status ${SERVICE_NAME}
 
-# service disable
-# systemctl disable "${SERVICE_NAME}"
 
 #/*---------------------------------------------------------------------------*/
 #/*---------------------------------------------------------------------------*/
